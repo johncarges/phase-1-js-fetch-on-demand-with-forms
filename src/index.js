6951e497e7c90b056c7b4149deb261eb0465ca28
John Carges <johncarges@gmail.com>
2023-07-17 11:22:52 -0400

    adds search functionality by book id

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const init = () => {
-  
+    const url = "http://localhost:3000/movies/"
+    const inputForm = document.getElementById('form')
+    
+    function renderSearchedMovie(movieInfo) {
+        const h4 = document.getElementById('movieDetailsTitle')
+        const p = document.getElementById('movieDetailsSummary')
+
+        h4.textContent = movieInfo.title
+        p.textContent = movieInfo.summary
+    }
+
+    inputForm.addEventListener("submit", (event)=> {
+        event.preventDefault()
+        const searchId = document.getElementById('searchByID').value
+        
+        fetch(url + searchId)
+            .then(r => r.json())
+            .then(renderSearchedMovie)
+
+    })
 }
+
+
 
 document.addEventListener('DOMContentLoaded', init);
